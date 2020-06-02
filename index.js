f752a214f9a53b623e79c26d68f1b374c87dfc49
Manjari <jlmanjari@gmail.com>
2020-06-02 12:40:02 -0300

    added files

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+var express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./db');
+
+
+// simple GET api
+app.get('/home', function (req, res) {
+    res.send("Hello world!");
+});
+
+// GET api to print results of employee table
+app.get('/home/employees', function (req, res) {
+    connection.query(
+        "USE employeesDB");
+    connection.query(
+        "SELECT * FROM `employee`",
+        function (error, results, fields) {
+            if (error) throw error;
+            res.json(results);
+        }
+    );
+});
+
+app.listen(3000, function () {
+    console.log("Listening on port 3000");
+});
+
+
+
